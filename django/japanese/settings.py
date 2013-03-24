9691aae390996b2485a41909affcb2793ef3d91f
Markus Schuss <markus.schuss@student.tugraz.at>
2013-03-24 21:53:40 +0100

    added twitter bootstrap to settings

--- django/japanese/settings.py
+++ django/japanese/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'twitter_bootstrap',
 )
 
 # A sample logging configuration. The only tangible logging
